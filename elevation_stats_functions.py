91b11f2bda9923ac09e5e13a16d0987d72c6676f
Aberry-byte <70750505+Aberry-byte@users.noreply.github.com>
2020-12-08 12:30:13 -0700

    Update elevation_stats_functions.py

--- elevation_stats_functions.py
+++ elevation_stats_functions.py
@@ -8,7 +8,7 @@ def append_to_array_from_file_regex(file, regex):
         for i, line in enumerate(file):
             for match in re.finditer(regex, line):
                 # print(f'Found on line {i+1}: {match.group()}')
-                match_line_list = MatchRegexLine(list(match.group()))
+                match_line_list = MatchRegexLine(list(str(match.group()).split()))
                 line_data = [match_line_list.point_id(), match_line_list.lat(), match_line_list.long(),
                              round(match_line_list.elevation(1000), 5), match_line_list.point_id_num()]
                 array.append(line_data)
@@ -83,51 +83,22 @@ class MatchRegexLine:
 
     def point_id(self):
         """Returns the point ID also know as the Label from bundleout file"""
-        point_ID = ''
-        point_list_position = 0
-        point_list_position_max = 9
-        while point_list_position <= point_list_position_max:
-            point_ID = point_ID + str(self.__matchlist[point_list_position])
-            point_list_position += 1
-        return str(point_ID)
+        return str(self.__matchlist[0])
 
     def point_id_num(self):
-        """Returns the point ID final 4 numbers from bundleout file"""
-        point_ID_num = ''
-        point_list_position = 5
-        point_list_position_max = 9
-        while point_list_position <= point_list_position_max:
-            point_ID_num = point_ID_num + str(self.__matchlist[point_list_position])
-            point_list_position += 1
-        return str(point_ID_num)
+        """Returns the point ID final numbers from bundleout file"""
+        split_point_id = str(self.__matchlist[0]).split('_')
+        return int(split_point_id[-1])
 
     def lat(self):
         """Returns the latitude of control point from given list from bundleout file"""
-        lat = ''
-        lat_list_position = 46
-        lat_list_position_max = 56
-        while lat_list_position <= lat_list_position_max:
-            lat = lat + str(self.__matchlist[lat_list_position])
-            lat_list_position += 1
-        return float(lat)
+        return float(self.__matchlist[6])
 
     def long(self):
         """Returns the longitude of control point from given list from bundleout file"""
-        long = ''
-        long_list_position = 62
-        long_list_position_max = 73
-        while long_list_position <= long_list_position_max:
-            long = long + str(self.__matchlist[long_list_position])
-            long_list_position += 1
-        return float(long)
+        return float(self.__matchlist[7])
 
     def elevation(self, multiplier=1):
         """Returns the elevation of control point from given list from bundleout file;
         :param multiplier: multiply your elevation data by any value to get your desired result"""
-        elev = ''
-        elev_list_position = 76
-        elev_list_position_max = 88
-        while elev_list_position <= elev_list_position_max:
-            elev = elev + str(self.__matchlist[elev_list_position])
-            elev_list_position += 1
-        return (float(elev) * multiplier)
+        return (float(self.__matchlist[8]) * multiplier)
